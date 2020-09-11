8ec712e32d4da587556ce7081fc7ff3ef2562211
Perry Luigi <Comments>
2020-09-10 19:05:32 -0500

    jwt done

--- App/src/main/java/com/eCommerce/App/AppApplication.java
+++ App/src/main/java/com/eCommerce/App/AppApplication.java
@@ -3,10 +3,8 @@ package com.eCommerce.App;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class AppApplication {
-
-	public static void main(String[] args) {
+@SpringBootApplication(scanBasePackages = {"com.eCommerce.App"})
+public class AppApplication { public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
 	}
 
---
+++ App/src/main/java/com/eCommerce/App/dao/UserDao.java
@@ -0,0 +1,12 @@
+package com.eCommerce.App.dao;
+
+import com.eCommerce.App.model.DAOUser;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserDao extends CrudRepository<DAOUser, Integer> {
+	
+	DAOUser findByUsername(String username);
+	
+}
